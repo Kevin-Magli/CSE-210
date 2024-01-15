e25d4eb9b05721fb64ebdc26bf1d1eee5b039cb0
kevin-magli <kmagliocchi@gmail.com>
2024-01-15 17:36:08 -0300

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,32 +5,54 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        
+
         List<int> numbers;
         numbers = new List<int>();
-        
-        
-        
+
+
+
         int number;
 
         do
         {
-            
+
             Console.Write("Enter a number: ");
             string userInput = Console.ReadLine();
-            number = int.Parse(userInput); 
-            numbers.Add(number); // this will also add 0, so deal with it.
+            number = int.Parse(userInput);
+            if (!(number == 0))
+                numbers.Add(number);
         } while (!(number == 0));
 
 
-        Console.WriteLine(numbers.Count); //this is counting how many items the list has.
-        
+        int sum = numbers.Sum();
+        int largest = numbers.Max();
+
+        double average = numbers.Average();
+
+        List<int> numbersSorted = new List<int>(numbers);
+        numbersSorted.Reverse();
+        int smallest = 999999999;
+        foreach (int i in numbersSorted)
+        {
+            if (i > 0 && i < smallest)
+                smallest = i;
+        }
+
+
+
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        Console.WriteLine($"The sorted list is:");
+
+        numbersSorted.Sort();
+        foreach (int i in numbersSorted)
+        {
+            Console.Write($"{i}, ");
+        }
     }
 }
-
-/* 
-The next step is to show the items inside the list, correct the "adding 0" problem and do the following:
-- find a way to compute the sum, the average and the largest number in the list.
-- add negative numbers also and add a option to find the smallest positive number
-- sort the numbers in the list and print them.
-*/
