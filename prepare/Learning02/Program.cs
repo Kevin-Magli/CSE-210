764a5d435eca1eb1ea35b3d3ede689d4f0efb2f2
kevin-magli <kmagliocchi@gmail.com>
2024-01-23 18:51:04 -0300

    Finished Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,58 @@
 using System;
+/*
+Step 1 Design classes:
 
+Class: Job
+Responsabilities: keep track of company, job title, start year and end year.
+Behaviors: Display information in format:  "Job Title (Company) StartYear-EndYear", 
+for example: "Software Engineer (Microsoft) 2019-2022".
+
+Class: Resume
+Responsabilities: keep track of persons name and a list of their jobs.
+Behaviors: Displays the resume in format: "Name, \n each job \n
+TIP: put the display in void
+*/
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = "2020",
+            _endYear = "2022"
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Web Developer",
+            _company = "Google",
+            _startYear = "2022",
+            _endYear = "2024"
+        };
+
+        job1.Display();
+        job2.Display();
+        
+
+        Resume myResume = new Resume
+        {
+            _name = "Kevin Magli",
+        };
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
+
+/*
+Class: Resume
+Responsabilities: keep track of persons name and a list of their jobs.
+Behaviors: Displays the resume in format: "Name, \n each job \n
+TIP: put the display in void
+*/
+
